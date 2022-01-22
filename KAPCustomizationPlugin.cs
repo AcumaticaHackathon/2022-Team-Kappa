4bf7e69c24ddbc6d98922ab67fb872f34a4547da
Robert Waite <rwaite@repay.com>
2022-01-22 16:29:50 -0700

    Initial Commit

---
+++ KAPCustomizationPlugin.cs
@@ -0,0 +1,28 @@
+﻿using Customization;
+using PX.Data;
+using PX.Data.BQL.Fluent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KAPPA
+{
+    public partial class KAPCustomizationPlugin : CustomizationPlugin
+    {
+
+        public override void UpdateDatabase()
+        {
+            PXTrace.WriteInformation("testig_Kappa_123");
+            WriteLog($"testig_Kappa_start");
+            ExecuteKappaDatabaseInitializer();
+            WriteLog($"testig_Kappa_end");
+        }
+
+        public void ExecuteKappaDatabaseInitializer()
+        {
+            PXTrace.WriteInformation("testig_Kappa_ExecuteKappaDatabaseInitializer");
+        }
+    }
+}
---
+++ KAPSalesOrderMaintExt.cs
@@ -0,0 +1,30 @@
+﻿using PX.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PX.Objects.SO
+{
+    // Acuminator disable once PX1016 ExtensionDoesNotDeclareIsActiveMethod extension should be constantly active
+    public class KAPSalesOrderMaintExt : PXGraphExtension<SOOrderEntry>
+    {
+        public PXAction<SOOrder> KAPTest;
+
+
+        ///<summary>
+        ///    This Provides a method for the user to navigate and or reference the ClickToPay link
+        ///</summary>
+        [PXButton]
+        [PXUIField(DisplayName = "Test Kappa")]
+        protected void kAPTest()
+        {
+            PXTrace.WriteInformation("testing kappa button");
+
+            var kapCustomizationPlugin = new KAPPA.KAPCustomizationPlugin();
+            kapCustomizationPlugin.ExecuteKappaDatabaseInitializer();
+
+        }
+    }
+}
