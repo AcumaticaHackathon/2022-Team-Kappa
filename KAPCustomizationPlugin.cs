26b673b725d17b896cebb7355b2f9a195aa16cd0
Robert Waite <rwaite@repay.com>
2022-01-22 18:17:00 -0700

    progress on getting meta data using reflection.

--- KAPCustomizationPlugin.cs
+++ KAPCustomizationPlugin.cs
@@ -4,6 +4,7 @@ using PX.Data.BQL.Fluent;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -23,6 +24,38 @@ namespace KAPPA
         public void ExecuteKappaDatabaseInitializer()
         {
             PXTrace.WriteInformation("testig_Kappa_ExecuteKappaDatabaseInitializer");
+            var test = typeof(KAPSampleDac);
+            foreach (var custAtb in test.CustomAttributes)
+            {
+                PXTrace.WriteInformation(custAtb.ToString());
+            }
+            var members = test.FindMembers(
+                MemberTypes.All, 
+                BindingFlags.Default,
+                new MemberFilter(DelegateToSearchCriteria),
+                "*");
+
+            foreach(var t in members)
+            {
+                PXTrace.WriteInformation($"Name:{t.Name} type:{t.MemberType}");
+
+            }
+
+            foreach(var t in test.GetProperties())
+            {
+                PXTrace.WriteInformation(t.ToString());
+            }
+
+            PXTrace.WriteInformation("test");
+            //foreach (var atb in test.FindMembers)
+            //{
+            //    PXTrace.WriteInformation(custAtb.ToString());
+            //}
+        }
+
+        private bool DelegateToSearchCriteria(MemberInfo m, object filterCriteria)
+        {
+            return true;
         }
     }
 }
--- KAPSampleDac.cs
+++ KAPSampleDac.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PX.Data;
+using PX.Data.BQL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,24 @@ using System.Threading.Tasks;
 
 namespace KAPPA
 {
-    class KAPSampleDac
+    public class KAPSampleDac : IBqlTable
     {
+        public abstract class kapString : BqlString.Field<kapString> { }
+        /// <summary>
+        /// Holds the code used as a primary key in the ClickToPay system
+        /// </summary>
+        [PXDBString(256, IsUnicode = true)]
+        [PXDefault("", PersistingCheck = PXPersistingCheck.Nothing)]
+        [PXUIField(DisplayName = "Kappa String", Enabled = false)]
+        public string KapString { get; set; }
+
+        public abstract class kapString2 : BqlString.Field<kapString2> { }
+        /// <summary>
+        /// Holds the code used as a primary key in the ClickToPay system
+        /// </summary>
+        [PXDBString(256, IsUnicode = true)]
+        [PXDefault("", PersistingCheck = PXPersistingCheck.Nothing)]
+        [PXUIField(DisplayName = "Kappa String2", Enabled = false)]
+        public string KapString2 { get; set; }
     }
 }
